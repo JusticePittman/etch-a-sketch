2647faf3104934775b0a83e635cec8dd5cb93003
Justice Pittman <justice.a.pittman@gmail.com>
2022-01-19 20:45:07 -0500

    Hover changes color, reset button added

--- etchSketch.js
+++ etchSketch.js
@@ -1,8 +1,22 @@
+const gridSize = 16;
 const gridDiv = document.querySelector('.grid');
+const gridReset = document.querySelector('.reset');
 
-for(let i = 1; i <= (16*16); i++) {
-    let gridItem = document.createElement('div');
-    gridItem.className = 'grid-item';
-    gridItem.style.aspectRatio = 1;
-    gridDiv.appendChild(gridItem);
+gridReset.addEventListener('click', () => {window.location.reload()});
+
+function makeGrid(cells) {
+    gridDiv.style.gridTemplateColumns = `repeat(${cells}, minmax(2em, 3.25em))`;
+    gridDiv.style.gridTemplateRows = `repeat(${cells}, minmax(2em, 3.25em))`;
+    
+    for(let i = 0; i < cells * cells; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+        gridItem.style.aspectRatio = 1;
+        gridItem.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = 'blue';
+        });
+        gridDiv.appendChild(gridItem);
+    };
 };
+
+makeGrid(gridSize);
